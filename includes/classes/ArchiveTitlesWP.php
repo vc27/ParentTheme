6474d73b157417c177dfe1c5bb520e13af7be6d7
Randy Hicks <randy@visualcoma.com>
2015-12-05 14:50:57 -0500

    v-7.9.0 randy-c-1.0
    - update: commit log and version notes
    - update theme options
    - remove comments completely if checked
    - add is_home to archive title

--- includes/classes/ArchiveTitlesWP.php
+++ includes/classes/ArchiveTitlesWP.php
@@ -16,46 +16,46 @@
  * @since 0.0.0
  **/
 class ArchiveTitlesWP {
-	
-	
-	
+
+
+
 	/**
 	 * description
-	 * 
+	 *
 	 * @access public
 	 * @var string
 	 * @since 0.0.0
 	 * Description: Term description
 	 **/
 	var $description = false;
-	
-	
-	
+
+
+
 	/**
 	 * errors
-	 * 
+	 *
 	 * @access public
 	 * @var array
 	 * @since 0.0.0
 	 **/
 	var $errors = array();
-	
-	
-	
+
+
+
 	/**
 	 * have_errors
-	 * 
+	 *
 	 * @access public
 	 * @var bool
 	 * @since 0.0.0
 	 **/
 	var $have_errors = 0;
-	
-	
-	
-	
-	
-	
+
+
+
+
+
+
 	/**
 	 * __construct
 	 * @since 0.0.0
@@ -63,85 +63,85 @@ class ArchiveTitlesWP {
 	function __construct() {
 
 	} // end function __construct
-	
-	
-	
-	
-	
-	
+
+
+
+
+
+
 	/**
 	 * set
 	 * @since 0.0.0
 	 **/
 	function set( $key, $val = false ) {
-		
+
 		if ( isset( $key ) AND ! empty( $key ) ) {
 			$this->$key = $val;
 		}
-		
+
 	} // end function set
-	
-	
-	
-	
-	
-	
+
+
+
+
+
+
 	/**
 	 * error
 	 * @since 0.0.0
 	 **/
 	function error( $error_key ) {
-		
+
 		$this->errors[] = $error_key;
-		
+
 	} // end function error
-	
-	
-	
-	
-	
-	
+
+
+
+
+
+
 	/**
 	 * get
 	 * @since 0.0.0
 	 **/
 	function get( $key ) {
-		
+
 		if ( isset( $key ) AND ! empty( $key ) AND isset( $this->$key ) AND ! empty( $this->$key ) ) {
 			return $this->$key;
 		} else {
 			return false;
 		}
-		
+
 	} // end function get
-	
-	
-	
-	
-	
-	
+
+
+
+
+
+
 	####################################################################################################
 	/**
 	 * Functionality
 	 **/
 	####################################################################################################
-	
-	
-	
-	
-	
-	
+
+
+
+
+
+
 	/**
 	 * get_title
 	 * @since 0.0.0
 	 **/
 	function get_title( $args = array() ) {
-		
+
 		$this->set_type();
 		if ( ! $this->have_type() ) {
 			return false;
 		}
-		
+
 		global $wp_query;
 		$defaults = array(
 			'class' => '',
@@ -149,24 +149,24 @@ class ArchiveTitlesWP {
 		);
 		$r = wp_parse_args( $args, $defaults );
 		extract( $r, EXTR_SKIP );
-		
+
 		$this->set_the_title();
 		$this->set_description();
 
 
 		$output = "<div class=\"archive-title $class\">";
-		
+
 			$output .= "<h1>" . apply_filters( 'ArchiveTitlesWP-title', $this->the_title, $this ) . "</h1>";
-			
+
 			if ( $this->have_description() ) {
 				$output .= "<div class=\"entry\">";
 					$output .= wpautop( $this->description );
 				$output .= '</div>';
 			}
-			
+
 		$output .= '</div>';
-		
-		
+
+
 		if ( $echo ) {
 			echo $output;
 		} else {
@@ -175,19 +175,21 @@ class ArchiveTitlesWP {
 
 
 	} // end function get_title
-	
-	
-	
-	
-	
-	
+
+
+
+
+
+
 	/**
 	 * set_type
 	 * @since 0.0.0
 	 **/
 	function set_type() {
 
-		if ( is_category() ) {
+		if ( is_home() ) {
+			$this->set( 'type', 'home' );
+		} else if ( is_category() ) {
 			$this->set( 'type', 'category' );
 		} else if ( is_tag() ) {
 			$this->set( 'type', 'tag' );
@@ -200,24 +202,24 @@ class ArchiveTitlesWP {
 		}
 
 
-	} // end function set_type 
-	
-	
-	
-	
-	
-	
+	} // end function set_type
+
+
+
+
+
+
 	/**
 	 * set_the_title
 	 * @since 0.0.0
 	 **/
 	function set_the_title() {
-		global $wp_query; 
-		
-		if ( 
-			isset( $wp_query->queried_object->taxonomy ) 
-			AND isset( $wp_query->queried_object->name ) 
-			AND ! empty( $wp_query->queried_object->name ) 
+		global $wp_query;
+
+		if (
+			isset( $wp_query->queried_object->taxonomy )
+			AND isset( $wp_query->queried_object->name )
+			AND ! empty( $wp_query->queried_object->name )
 		) {
 			$this->set( 'the_title', $wp_query->queried_object->name );
 		} else if ( $this->type == 'date' ) {
@@ -234,108 +236,113 @@ class ArchiveTitlesWP {
 			$this->set( 'the_title', $the_title );
 		} else if ( $this->type == 'author' ) {
 			$this->set( 'the_title', "Author " . $wp_query->queried_object->data->display_name );
+		} else if ( $this->type == 'home' ) {
+			$this->set( 'the_title', get_the_title( get_option( 'page_for_posts' ) ) );
 		}
-		
+
 	} // end function set_the_title
-	
-	
-	
-	
-	
-	
+
+
+
+
+
+
 	/**
 	 * set_description
 	 * @since 0.0.0
 	 **/
 	function set_description() {
-		global $wp_query; 
-		
-		if ( 
-			isset( $wp_query->queried_object->taxonomy ) 
-			AND isset( $wp_query->queried_object->description ) 
-			AND ! empty( $wp_query->queried_object->description ) 
+		global $wp_query;
+
+		if (
+			isset( $wp_query->queried_object->taxonomy )
+			AND isset( $wp_query->queried_object->description )
+			AND ! empty( $wp_query->queried_object->description )
 		) {
 			$this->set( 'description', $wp_query->queried_object->description );
 		} else if ( $this->type == 'author' ) {
 			$this->set( 'description', get_the_author_meta( 'description', $wp_query->queried_object->data->ID ) );
+		} else if ( $this->type == 'home' ) {
+			// is it worth the query to get the page content?
+			// $this->set( 'description', '');
 		}
-		
+
 	} // end function set_description
-	
-	
-	
-	
-	
-	
+
+
+
+
+
+
 	####################################################################################################
 	/**
 	 * Conditionals
 	 **/
 	####################################################################################################
-	
-	
-	
-	
-	
-	
+
+
+
+
+
+
 	/**
 	 * have_errors
 	 * @since 0.0.0
 	 **/
 	function have_errors() {
-		
+
 		if ( isset( $this->errors ) AND ! empty( $this->errors ) AND is_array( $this->errors ) ) {
 			$this->set( 'have_errors', 1 );
 		} else {
 			$this->set( 'have_errors', 0 );
 		}
-		
+
 		return $this->have_errors;
-		
-	} // end function have_errors 
-	
-	
-	
-	
-	
-	
+
+	} // end function have_errors
+
+
+
+
+
+
 	/**
 	 * have_description
 	 * @since 0.0.0
 	 **/
 	function have_description() {
-		
+
 		if ( isset( $this->description ) AND ! empty( $this->description ) ) {
 			$this->set( 'have_description', 1 );
 		} else {
 			$this->set( 'have_description', 0 );
 		}
-		
+
 		return $this->have_description;
-		
+
 	} // end function have_description
-	
-	
-	
-	
-	
-	
+
+
+
+
+
+
 	/**
 	 * have_type
 	 * @since 0.0.0
 	 **/
 	function have_type() {
-		
+
 		if ( isset( $this->type ) AND ! empty( $this->type ) ) {
 			$this->set( 'have_type', 1 );
 		} else {
 			$this->set( 'have_type', 0 );
 		}
-		
+
 		return $this->have_type;
-		
+
 	} // end function have_type
-	
-	
-	
+
+
+
 } // end class ArchiveTitlesWP
